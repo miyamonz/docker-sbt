ca9cf127f0ce5054ed7cf98995c97037eaa70a31
Koichi Miyamoto <m5813k@gmail.com>
2021-09-30 19:03:15 +0900

    add openjdk version

--- genMatrix.js
+++ genMatrix.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 const generateBuildMatrix = () => {
   const entries = [
+    {OPENJDK_TAG: "8u302", SBT_VERSION: "0.13.17"},
     {OPENJDK_TAG: "8u292", SBT_VERSION: "0.13.17"},
   ]
 
